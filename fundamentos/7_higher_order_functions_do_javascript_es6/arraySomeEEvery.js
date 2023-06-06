e8d3e44b0036e88acf58ae62500042683547edba
brunoloubraz <brunolou.braz@gmail.com>
2023-06-06 14:07:09 -0300

    realizando exercicios

--- fundamentos/7_higher_order_functions_do_javascript_es6/arraySomeEEvery.js
+++ fundamentos/7_higher_order_functions_do_javascript_es6/arraySomeEEvery.js
@@ -1,14 +1,3 @@
-// // const pessoas = [
-// //   { nome: 'Ana', cargo: 'Analista' },
-// //   { nome: 'João', cargo: 'Gerência' },
-// //   { nome: 'Aline', cargo: 'Analista' },
-// //   { nome: 'Joana', cargo: 'Gerência' },
-// // ];
-
-// // const verificaCargo = pessoas.some((pessoa) => pessoa.cargo === 'Gerência');
-
-// // console.log(verificaCargo); // true
-
 // const pessoas = [
 //   { nome: 'Ana', cargo: 'Analista' },
 //   { nome: 'João', cargo: 'Gerência' },
@@ -16,30 +5,41 @@
 //   { nome: 'Joana', cargo: 'Gerência' },
 // ];
 
-// const verificaCargo = pessoas.some((pessoa) => pessoa.cargo === 'Product Owner');
+// const verificaCargo = pessoas.some((pessoa) => pessoa.cargo === 'Gerência');
 
-// console.log(verificaCargo); // false
+// console.log(verificaCargo); // true
+
+const pessoas = [
+  { nome: 'Ana', cargo: 'Analista' },
+  { nome: 'João', cargo: 'Gerência' },
+  { nome: 'Aline', cargo: 'Analista' },
+  { nome: 'Joana', cargo: 'Gerência' },
+];
+
+const verificaCargo = pessoas.some((pessoa) => pessoa.cargo === 'Product Owner');
+
+console.log(verificaCargo); // false
 
 
-// const listNames = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+const listNames = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
 
-// const verifyFirstLetter = (letter, names) => names.some((name) => name[0] === letter);
+const verifyFirstLetter = (letter, names) => names.some((name) => name[0] === letter);
 
-// console.log(verifyFirstLetter('J', listNames)); // true
-// console.log(verifyFirstLetter('X', listNames)); // false
-
-
+console.log(verifyFirstLetter('J', listNames)); // true
+console.log(verifyFirstLetter('X', listNames)); // false
 
 
-// const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
 
-// const hasName = (arr, name) => {
-//   //Adicione seu código aqui
-//   return arr.some((nome) => nome === name)
-// };
 
-// console.log(hasName(names, 'Ana'));
-// console.log(hasName(names, 'Pedro'));
+const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => {
+  //Adicione seu código aqui
+  return arr.some((nome) => nome === name)
+};
+
+console.log(hasName(names, 'Ana'));
+console.log(hasName(names, 'Pedro'));
 
 const people = [
   { name: 'Mateus', age: 18 },
--- fundamentos/7_higher_order_functions_do_javascript_es6/exercicios/parte1.js
+++ fundamentos/7_higher_order_functions_do_javascript_es6/exercicios/parte1.js
@@ -1,36 +1,88 @@
 
-// const newEmployees = (func) => {
+// // const newEmployees = (func) => {
+// //   const employees = {
+// //     id1: `${func('Pedro Guerra')}`, // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+// //     id2: `${func('Luiza Drumond')}`, // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+// //     id3: `${func('Carla Paiva')}`, // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+// //   }
+// //   return employees;
+// // };
+
+// // const email = (name) => {
+// //   return `${name}@trybe.com`
+// // }
+
+
+// // // const mail = emails.forEach((email,index) => {
+// // //   emails[index] = `${emails[index]}@trybe.com.br`
+// // // });
+
+// // console.log(newEmployees(email));
+
+// const employeeGenerator = (fullName) => {
+//   const email = fullName.toLowerCase().replace(' ', '_');
+//   return { fullName, email: `${email}@trybe.com` };
+// };
+
+// const newEmployees = (callback) => {
 //   const employees = {
-//     id1: `${func('Pedro Guerra')}`, // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-//     id2: `${func('Luiza Drumond')}`, // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-//     id3: `${func('Carla Paiva')}`, // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
-//   }
+//     id1: callback('Pedro Guerra'),
+//     id2: callback('Luiza Drumond'),
+//     id3: callback('Carla Paiva'),
+//   };
 //   return employees;
 // };
 
-// const email = (name) => {
-//   return `${name}@trybe.com`
-// }
+// console.log(newEmployees(employeeGenerator));
+
+// //SORTEADOR DE LOTERIA
+
+// // const sorteio = (numero) => {
+// //   const randomNumber = Math.floor(Math.random() * (6 - 1) + 1)
+// //   if (numero === randomNumber) {
+// //     return 'Parabéns, você ganhou'
+// //   }
+// //   else {
+// //     return 'Tente novamente'
+// //   }
+// // }
+
+// // console.log(sorteio(5));
+
+// const numberChecker = (myNumber, number) => myNumber === number;
+
+// const lotteryResult = (myNumber, callback) => {
+//   const number = Math.floor((Math.random() * 5) + 1);
+
+//   return callback(myNumber, number) ? 'Lucky day, you won!' : 'Try Again!';
+// };
+
+// console.log(lotteryResult(2, numberChecker));
 
 
-// // const mail = emails.forEach((email,index) => {
-// //   emails[index] = `${emails[index]}@trybe.com.br`
-// // });
 
-// console.log(newEmployees(email));
+// //Corretor automático de exame
 
-const employeeGenerator = (fullName) => {
-  const email = fullName.toLowerCase().replace(' ', '_');
-  return { fullName, email: `${email}@trybe.com` };
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+
+const corretor = (rightAnswer, answers, action) => {
+  let soma = 0;
+  for (let index = 0; index < rightAnswer.length; index += 1) {
+    soma += action(rightAnswer[index], answers[index]);
+
+  }
+  return soma
+}
+
+const compare = (rightAnswer, studentAnswer) => {
+  if (rightAnswer === studentAnswer) {
+    return 1;
+  } if (studentAnswer === 'N.A') {
+    return 0;
+  }
+  return -0.5;
 };
 
-const newEmployees = (callback) => {
-  const employees = {
-    id1: callback('Pedro Guerra'),
-    id2: callback('Luiza Drumond'),
-    id3: callback('Carla Paiva'),
-  };
-  return employees;
-};
-
-console.log(newEmployees(employeeGenerator));
+console.log(corretor(RIGHT_ANSWERS, STUDENT_ANSWERS, compare));
