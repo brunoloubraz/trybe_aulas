cc37f0d70cc98322d3eb81073e137980f4a13a07
brunoloubraz <brunolou.braz@gmail.com>
2023-06-13 15:41:36 -0300

    default parameters

---
+++ fundamentos/7_higher_order_functions_do_javascript_es6/dia05_Array_destructuring,_Default_destructuring,_Object_property_shorthand_e_default_parameter/defaultParameters.js
@@ -0,0 +1,9 @@
+const greeting = (user = 'pessoa usuária') => console.log(`Welcome ${user}!`);
+
+greeting(); // // Welcome pessoa usuária!
+
+// ------
+
+const multiply = (number, value = 1) => number * value;
+
+console.log(multiply(8));
