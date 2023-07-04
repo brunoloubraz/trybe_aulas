29df5b588bdcbdb1974291dab878c1fcf2e01245
brunoloubraz <brunolou.braz@gmail.com>
2023-07-03 22:31:18 -0300

    conteudo e exercicio finalizados

---
+++ front-end/2_introducao_ao_react/dia02_componentes_react_e_props/typescript.ts
@@ -0,0 +1,43 @@
+// let username = 'tryber';
+// let age = 21;
+// let isStudent = true;
+
+// function myFunction() {
+//   console.log('This is a function');
+// }
+
+// let age = 21;
+// age = 'My age';
+
+// function sum(param1: number, param2: number) {
+//   return param1 + param1;
+// }
+
+// // função que retorna um número
+// function returnNumber(): number {
+//   // ...
+// }
+
+// // função que retorna um texto
+// function returnString(): string {
+//   // ...
+// }
+
+// // função que não tem nenhum retorno
+// function noReturn(): void {
+//   // ...
+// }
+
+// let arrayOfStrings: string[]; // armazenará uma lista de strings
+// let arrayOfNumbers: number[]; // armazenará uma lista de numbers
+// let arrayOfStringsOrNumbers: (string | number)[]; // armazenará uma lista de strings E/OU numbers
+// let arrayOfObjects: object[]; // armazenará uma lista de objetos
+
+
+type User = {
+  id: number,
+  name: string,
+  modules: string[],
+}
+
+let myUser: User;
