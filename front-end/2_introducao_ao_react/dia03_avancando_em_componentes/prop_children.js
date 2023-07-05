1ead4d2d00315d8b5d02726babd62ce589cdc01c
brunoloubraz <brunolou.braz@gmail.com>
2023-07-04 21:42:20 -0300

    conteudo finalizados

---
+++ front-end/2_introducao_ao_react/dia03_avancando_em_componentes/prop_children.js
@@ -0,0 +1,29 @@
+// type ButtonProps = {
+//   title: string
+// }
+
+// function Button({ title }: ButtonProps) {
+//   return (
+//     <button>
+//       { title }
+//     </button>
+//   )
+// }
+
+// export default Button;
+
+
+// type ButtonProps = {
+//   children: React.ReactNode
+// }
+
+// function Button({ children }: ButtonProps) {
+//   return (
+//     <button>
+//       { children }
+//     </button>
+//   )
+// }
+
+// export default Button;
+
