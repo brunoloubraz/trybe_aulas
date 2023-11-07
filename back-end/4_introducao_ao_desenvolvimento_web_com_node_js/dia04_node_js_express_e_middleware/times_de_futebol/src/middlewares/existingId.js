a349d32e227c642cc415c54763956c0f0cece2c6
brunoloubraz <brunolou.braz@gmail.com>
2023-11-07 19:24:58 -0300

    conteudo finalizado

---
+++ back-end/4_introducao_ao_desenvolvimento_web_com_node_js/dia04_node_js_express_e_middleware/times_de_futebol/src/middlewares/existingId.js
@@ -0,0 +1,11 @@
+const teams = require('../utils/teams')
+
+const existingId = (req, res, next) => {
+  const id = Number(req.params.id);
+  if (!teams.some((t) => t.id === id)) {
+    return res.sendStatus(404).json({ message: 'Time não encontrado' });
+  }
+  next();
+};
+
+module.exports = existingId;
