6bf1576294d5786a2c6d9774bb368db1a95cf8a7
brunoloubraz <brunolou.braz@gmail.com>
2024-03-06 19:41:23 -0300

    conteudo

---
+++ ciencia-da-computacao/3_listas_lineares/dia02_no_e_listas_encadeadas/node.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, value):
+        self.value: value
+        self.next = None
+  
+    def __str__(self):
+        return f"Node(value={self.value}, next={self.next})"
+    
+    def __len__(self):
+        return self.__length
